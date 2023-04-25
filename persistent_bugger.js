830c84a3d53cb66e0a22e9bf96e4258934820803
Rohan <rohanmorar@gmail.com>
2023-04-24 23:30:07 -0500

    04/24/23 - persistent bugger

---
+++ persistent_bugger.js
@@ -0,0 +1,43 @@
+/*
+
+Persistent Bugger (6 kyu)
+
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+Example 1:
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+
+Example 2:
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+
+Example 3:
+4 --> 0 (because 4 is already a one-digit number)
+
+*/
+
+/* Recursive helper function that returns the product result of multiplying the digits of a number together
+
+>>> multiplyDigits(43) 
+12
+
+*/
+function multiplyDigits(num){
+  if (num <= 9){
+    return num
+  }
+  else {
+    return num % 10 * multiplyDigits(Math.floor(num / 10))
+  }
+}
+
+/*
+Recursive function that returns the count of the multiplicative persistence.
+*/
+function persistence(num) {
+  if (num <= 9){
+    return 0
+  }
+  else{
+    return 1 + persistence(multiplyDigits(num))
+  }
+}

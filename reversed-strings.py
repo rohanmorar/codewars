48b4b5d0015db8e26e8672057645c86a10b58ea8
Rohan <rohanmorar@gmail.com>
2023-05-05 22:39:45 -0500

    05/05/23 codewars

---
+++ reversed-strings.py
@@ -0,0 +1,12 @@
+"""
+8 kyu - Reversed Strings
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+"""
+
+def solution(string):
+    return string[::-1]

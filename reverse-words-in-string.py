040c40449f45aae1e5618f204c2c845132381f65
Rohan <rohanmorar@gmail.com>
2023-05-19 23:06:52 -0500

    05/19/23 codewars

--- reverse-words-in-string.py
+++ reverse-words-in-string.py
@@ -4,7 +4,26 @@ Example: "A Wild Bat" ---> reverseString("A Wild Bat") ---> "Bat Wild A"
 
 >>> reverseString("A Big Dog Sat On The Roof")
 'Roof The On Sat Dog Big A'
+
+        P : 1 input - type: String
+        R : 1 output - type: String
+        E :
+            s = "A Bat Flew High" -> reverseWords(s) -> "High Flew Bat A"
+            s = "A    Bat  Flew  High" -> reverseWords(s) -> "High Flew Bat A"
+            s = "   A Bat  Flew   High  " -> reverseWords(s) -> "High Flew Bat A"
+        P :
+            s = "A Bat Flew High" -> reverseWords(s) -> "High Flew Bat A"
+                s.split() --->  ["A", "Bat", "Flew", "High"]
+                ["A", "Bat", "Flew", "High"][::-1] ---> ["High", "Flew", "Bat", "A"]
+                " ".join(["High", "Flew", "Bat", "A"]) ---> "High Flew Bat A"
+        A :
+            s.split() - using default split() e.g. no param passed, is O(n), since it just scans through the string finding all whitespace.
+            [::-1] - O(N), scanning each item from end to beginning
+            " ".join() - O(N), go through each item in array
+            Overall O(3 * N) ---> O(N) or Linear time complexity
+            Space Complexity: O(1) since we are not creating new arrays or maps
+
 """
 
 def reverseString(s):
-    " ".join(s.strip().split()[::-1])
+    return " ".join(s.split()[::-1])
